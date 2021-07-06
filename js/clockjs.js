d8cc75f60aa90210daafacc44cf91eafc9352c04
PoeEiEiThaw <poeeieithaw@ucstaungoo.edu.mm>
2021-07-06 22:39:09 +0630

    cleanerror

--- js/clockjs.js
+++ js/clockjs.js
@@ -6,11 +6,11 @@ function setClock(){
     const currentdate = new Date();
     const secondratio = currentdate.getSeconds()/60;
     const minuteratio = (secondratio + currentdate.getMinutes())/60;
-    const hourratio = (minuteratio + currentdate.getHours())/60;
+    const hourratio = (minuteratio + currentdate.getHours())/12;
 
     secondhand.style.setProperty("--rotation",secondratio*360);
     minutehand.style.setProperty("--rotation",minuteratio*360);
-    hourhand.style.setProperty("rotation",hourratio*360);
+    hourhand.style.setProperty("--rotation",hourratio*360);
 }
 setClock();
 
