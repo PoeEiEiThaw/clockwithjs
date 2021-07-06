87caeb47e9bf1dea91973e0b00c6bcdd7210b77a
PoeEiEiThaw <poeeieithaw@ucstaungoo.edu.mm>
2021-07-06 22:27:54 +0630

    clockjs

---
+++ js/clockjs.js
@@ -0,0 +1,17 @@
+setInterval(setClock,1000);
+const secondhand = document.querySelector('.hand.second');
+const minutehand = document.querySelector('.hand.minute');
+const hourhand = document.querySelector('.hand.hour');
+function setClock(){
+    const currentdate = new Date();
+    const secondratio = currentdate.getSeconds()/60;
+    const minuteratio = (secondratio + currentdate.getMinutes())/60;
+    const hourratio = (minuteratio + currentdate.getHours())/60;
+
+    secondhand.style.setProperty("--rotation",secondratio*360);
+    minutehand.style.setProperty("--rotation",minuteratio*360);
+    hourhand.style.setProperty("rotation",hourratio*360);
+}
+setClock();
+
+
